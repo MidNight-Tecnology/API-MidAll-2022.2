f1f002a14dc5b553e25b846a077df0cb5b776a40
Guilhermedcdias <guilhermedcdias.2021@gmail.com>
2022-09-12 07:30:18 -0300

    Adicionado Tela de Alterar emails e alterações no back

--- src/Back/src/routes/routes.ts
+++ src/Back/src/routes/routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import userControllers from "./controllers/userControllers";
+import userControllers from "../controllers/userControllers";
 
 const router = express.Router();
 
