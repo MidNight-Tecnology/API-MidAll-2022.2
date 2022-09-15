ffbf70f7954684a510858de57d85357d9cf33c28
Guilhermedcdias <guilhermedcdias.2021@gmail.com>
2022-09-15 00:49:20 -0300

    Renders Finalizandos

--- src/Front/routes/login.js
+++ src/Front/routes/login.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
+
+//RENDER
 router.get('/', (req, res) => {
     res.render("login")
 })
 
+
+//Redirect
 router.post('/clipping', (req, res) => {
     res.redirect('sistema/clipping')
-})
+ })
 
 
 
--- src/Front/routes/sistema.js
+++ src/Front/routes/sistema.js
@@ -1,10 +1,23 @@
 const express = require("express")
 const router = express.Router()
 
+
+//Renders -- AMEM DEU CERTO
 router.get('/clipping', (req, res) => {
     res.render('clipping')
 })
+router.get('/alterar_emails', (req, res) => {
+    res.render('alterar_emails')
+})
+router.get('/cadastro_assoc', (req, res) => {
+    res.render('cadastro_assoc')
+})
+router.get('/gerenc_assoc', (req, res) => {
+    res.render('gerenc_assoc')
+})
 
+
+//Redirects -- FOCO, FORÇA E FÉ
 
 
 
